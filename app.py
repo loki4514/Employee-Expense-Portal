4ed02c3d73fa1b571fb2dc41c4f0dfb35b28d54f
lokesh4514 <60394592+lokesh4514@users.noreply.github.com>
2023-04-12 16:02:08 +0530

    complete

--- app.py
+++ app.py
@@ -1,32 +1,20 @@
-from flask import Flask,render_template
-
+from flask import Flask,render_template,flash,redirect,url_for
+from forms import RegistrationForm,LoginForm
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '8e7b334da419398c10724c396620c7c4'
 
 
-posts = [
-    
-    {
-        'author' : "abc",
-        "no of post" : 1
-    },
-    {
-        'author' : "jgj",
-        "no of post" : 2
-    }
-    
-]
 @app.route("/")
-@app.route("/home")
-def home():
+@app.route("/index")
+def index():
     return render_template('index.html')
 
 
 @app.route("/admin")
 def admin():
-    return render_template('admin.html',posts=posts)
+    return render_template('admin.html')
 
 @app.route("/employee")
 def employee():
@@ -35,6 +23,37 @@ def employee():
 @app.route("/manager")
 def manager():
     return render_template('manager.html')
+
+
+# Create Employee details
+@app.route("/create", methods=["GET","POST"])
+def create():
+    # creating a registration form passing as an instance
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!",'success')
+        return redirect(url_for('admin'))
+    
+    return render_template('create.html',title='Create Employee',form=form)
+
+@app.route("/update")
+def update():
+    
+    return render_template('update.html')
+
+@app.route("/login", methods=["GET","POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.username.data == 'loki' and form.password.data == '1234':
+            flash('Sucessfully logged in')
+            return redirect(url_for('admin'))
+        else:
+            flash('Invalid username or password.', 'danger')
+    
+    return render_template('login.html', title='Login', form=form)
+
+
 
 
 if __name__ == '__main__':
--- forms.py
+++ forms.py
@@ -1,23 +1,23 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField,BooleanField
+from wtforms import StringField,PasswordField,SubmitField,BooleanField,EmailField
 from wtforms.validators import DataRequired,Length,Email,EqualTo
 
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('username',validators=[DataRequired(),Length(2,20)])
-    name = StringField('username',validators=[DataRequired(),Length(2,100)])
-    email = StringField('email',validators=[DataRequired(),Email])
+    username = StringField('User Name',validators=[DataRequired(),Length(2,20)])
+    name = StringField('Employee Name',validators=[DataRequired(),Length(2,100)])
+    email = EmailField('Employee Email',validators=[DataRequired(),Email()])
     
     password = PasswordField('Password',validators=[DataRequired()])
-    confirm_password = PasswordField('password',validators=[DataRequired(),EqualTo('password')])
+    confirm_password = PasswordField('Confirm password',validators=[DataRequired(),EqualTo('password')])
     
     submit = SubmitField('Create Employee')
     
     
 class LoginForm(FlaskForm):
-    username = StringField('username',validators=[DataRequired(),Length(2,20)])
-    remember = BooleanField('None')
+    username = StringField('User Name',validators=[DataRequired(),Length(2,20)])
+    remember = BooleanField('Remember Me')
     password = PasswordField('Password',validators=[DataRequired()])
     submit = SubmitField('Login')
     
